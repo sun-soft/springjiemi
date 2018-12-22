bcb0e0695f132f6cd05a5aaa8e23450bf35e4055
wenc-xiaoxin <wenc@wenhq.com>
2018-12-22 10:24:52 +0800

    init project

---
+++ src/main/java/com/wenhq/spring/springjiemi/App.java
@@ -0,0 +1,13 @@
+package com.wenhq.spring.springjiemi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
